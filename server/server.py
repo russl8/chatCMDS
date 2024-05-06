74ba6c93e722435c2c33ca36fe3339655452c781
russl8 <russl8@my.yorku.ca>
2024-05-05 21:07:04 -0400

    g

--- server/server.py
+++ server/server.py
@@ -1,19 +1,28 @@
-from flask import Flask
-from flask import request
+import os
+from flask import Flask, jsonify, request
+from os import environ
+from openai import OpenAI
+from flask_cors import CORS
+import json
+from apikey import apikey
 app = Flask(__name__)
 
-#members api route
-@app.route("/members")
-def members():
-    return {"members":["1","2","3"]}
+CORS(app)
+# venv: venv\Scripts\activate
+# how to delete environments??!
+# todo: setup anaconda
+os.environ["OPENAI_API_KEY"] = apikey
 
-@app.route("/message", methods=['GET'])
-def message():
-    print('wd')
-    if request.method == 'GET':
-        print("jo")
+@app.route("/predict", methods=["POST"])
+def predict():
+    existing_messages = json.loads(request.form["existingMessages"])
+    print(existing_messages)
+    client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+    completion = client.chat.completions.create(
+        model='gpt-3.5-turbo',
+        messages=existing_messages)
+    return jsonify({"result": completion.choices[0].message.content})
 
-    return {"members":["1","2","3"]}
 
-if __name__ == "__main__":
-    app.run(debug= True)
+if __name__ == '__main__':
+    app.run(debug=True)
